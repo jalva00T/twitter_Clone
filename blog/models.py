5bf50640247bb65bc070d7d005dce4d104c977b0
Jake Alvarez <color.feed91@gmail.com>
2021-05-25 15:57:15 -0500

    commented out like in models.py

--- blog/models.py
+++ blog/models.py
@@ -16,9 +16,9 @@ class Tweet(models.Model):
     image = CloudinaryField(
         'image', blank=True, null=True
     )
-    like_count = models.PositiveIntegerField(
-       default= 0, null=True
-    )
+    #like_count = models.PositiveIntegerField(
+    #   default= 0, null=True
+    #)
     created_at = models.DateTimeField(
         'created_datetime', blank=True, auto_now_add=True
     )
