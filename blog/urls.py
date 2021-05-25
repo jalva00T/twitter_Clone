e44f7e486c66fafce83dac318b3c3537047e7777
Jake Alvarez <color.feed91@gmail.com>
2021-05-25 16:40:06 -0500

    changed 'index' back to 'home' in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('tweetEdit/<int:tweet_id>', views.tweetEdit, name='tweetEdit'),
     path('tweetDelete/<int:tweet_id>', views.tweetDelete, name='tweetDelete'),
     path('tweetLike/<int:tweet_id>', views.tweetLike, name='tweetLike'),
